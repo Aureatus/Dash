fc4a33d722ac36d88b9195b648fb42b8dd4fc42b
Alex Knott <alextknott@gmail.com>
2022-07-27 17:32:33 +0100

    Create ThemeProvider that provides theme state
    Theme state is set on mount of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { darkTheme, lightTheme } from './themes';
+
 function App() {
-  return <div></div>;
+  const [theme, setTheme] = useState({});
+
+  useEffect(() => {
+    setTheme(darkTheme);
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <div></div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
