2944a7dcc21b57cfcd7da952e25e14f5ed8f66c5
Alex Knott <alextknott@gmail.com>
2022-07-27 17:02:40 +0100

    Create a module where some themes are defined

---
+++ src/themes.ts
@@ -0,0 +1,26 @@
+export const lightTheme = {
+  background: {
+    primary: '#ffffff',
+  },
+  text: {
+    primary: '#000000',
+  },
+
+  accent: {
+    primary: '#FFDA44',
+    secondary: '#d63131',
+  },
+};
+
+export const darkTheme = {
+  background: {
+    primary: '#000000',
+  },
+  text: {
+    primary: 'ffffff',
+  },
+  accent: {
+    primary: '#FFDA44',
+    secondary: '#d63131',
+  },
+};
