aa3a16f3b7562b5ee98152636540dc2a65726b84
Alex Knott <alextknott@gmail.com>
2022-07-28 13:55:35 +0100

    Set global font-family Lato on body

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     color: ${({ theme }) => theme.text.primary};
     background-color: ${({ theme }) => theme.background.primary};
-
+    font-family: 'Lato', sans-serif;
   }
 `;
 
