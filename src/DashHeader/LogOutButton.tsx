f9bea8f1143750f22a2d5d9fa5e01bd9a24ceec8
Alex Knott <alextknott@gmail.com>
2022-07-28 15:09:04 +0100

    Create LogOutButton component
    Fully styled, but not functional as of yet.

---
+++ src/DashHeader/LogOutButton.tsx
@@ -0,0 +1,12 @@
+import LogoutSVG from '..//..//public/logout_FILL0_wght200_GRAD0_opsz40.svg';
+import { LogoutImg, StyledButton } from './StyledComponents';
+
+const LogOutButton = () => {
+  return (
+    <StyledButton>
+      <LogoutImg src={LogoutSVG} />
+    </StyledButton>
+  );
+};
+
+export default LogOutButton;
