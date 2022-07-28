524a58abd4143c2bf54416d56f947ff69fe59a15
Alex Knott <alextknott@gmail.com>
2022-07-28 12:42:00 +0100

    Add GlobalStyle to render of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import GlobalStyle from './GlobalStyle';
 import { darkTheme, lightTheme } from './themes';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div></div>
     </ThemeProvider>
   );
