ba8564f89e85f314f7c87b2d1313df4e91ace993
Alex Knott <alextknott@gmail.com>
2022-07-28 12:44:15 +0100

    Create module with general styled components

--- src/StyledComponents.ts
+++ src/StyledComponents.ts
@@ -1,1 +1,12 @@
-export {};
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: grid;
+
+  grid-template: 1fr 10fr / 1fr;
+`;
+
+export { Container };
