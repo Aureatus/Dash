223c80563cf64aa5509e7be0df97c2d19b105d11
Alex Knott <alextknott@gmail.com>
2022-07-29 18:30:41 +0100

    Create module that exports firebase app

---
+++ src/firebase/app/app.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXXHWX482_xbNisLx441aUKoGi0U-J6ys',
+  authDomain: 'dash-6c33e.firebaseapp.com',
+  projectId: 'dash-6c33e',
+  storageBucket: 'dash-6c33e.appspot.com',
+  messagingSenderId: '904985093481',
+  appId: '1:904985093481:web:eba272f6be279995c849f8',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
