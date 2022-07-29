df5058d716703d79ade56b532ff72d061f0d5b4b
Alex Knott <alextknott@gmail.com>
2022-07-29 16:08:16 +0100

    Set up Routes containing an index route, and a dashboard route that is protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import DashHeader from './Components/DashHeader/DashHeader';
+import ProtectedRoute from './Components/ProtectedRoute';
 import GlobalStyle from './GlobalStyle';
 import { Container } from './StyledComponents';
 import { darkTheme, lightTheme } from './themes';
@@ -15,7 +16,12 @@ function App() {
       <GlobalStyle />
       <Container>
         <DashHeader currentUser={null} setTheme={setTheme}></DashHeader>
-        <Routes></Routes>
+        <Routes>
+          <Route index element={<Navigate to={'/landing'} />} />
+          <Route element={<ProtectedRoute currentUser={null} redirectPath="landing" />}>
+            <Route path="dashboard" element={<div>test</div>} />
+          </Route>
+        </Routes>
       </Container>
     </ThemeProvider>
   );
