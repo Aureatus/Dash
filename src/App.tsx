517ccbd026d2ea965a6665ea5fd91800bf1ff9a6
Alex Knott <alextknott@gmail.com>
2022-08-01 15:56:06 +0100

    Remove currentUser prop from DashHeader

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,31 @@ import { ThemeProvider } from 'styled-components';
 
 import DashHeader from './Components/DashHeader/DashHeader';
 import ProtectedRoute from './Components/ProtectedRoute';
+import UserContext from './context/UserContext';
 import GlobalStyle from './GlobalStyle';
+import useAuthState from './hooks/useAuthState';
 import { Container } from './StyledComponents';
 import { darkTheme, lightTheme } from './themes';
 
 function App() {
   const [theme, setTheme] = useState(lightTheme);
 
+  const [user, loading, error] = useAuthState();
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Container>
-        <DashHeader currentUser={null} setTheme={setTheme}></DashHeader>
-        <Routes>
-          <Route index element={<Navigate to={'/landing'} />} />
-          <Route element={<ProtectedRoute currentUser={null} redirectPath="landing" />}>
-            <Route path="dashboard" element={<div>test</div>} />
-          </Route>
-        </Routes>
-      </Container>
+      <UserContext.Provider value={user}>
+        <Container>
+          <DashHeader setTheme={setTheme}></DashHeader>
+          <Routes>
+            <Route index element={<Navigate to={'/landing'} />} />
+            <Route element={<ProtectedRoute currentUser={null} redirectPath="landing" />}>
+              <Route path="dashboard" element={<div>test</div>} />
+            </Route>
+          </Routes>
+        </Container>
+      </UserContext.Provider>
     </ThemeProvider>
   );
 }
