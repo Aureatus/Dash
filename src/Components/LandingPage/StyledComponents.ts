c7cc1e4f07b3e5ca0f1525a18158743c4e1e3bcd
Alex Knott <alextknott@gmail.com>
2022-08-02 11:16:06 +0100

    Create Styled components module for LandingPage containing container

---
+++ src/Components/LandingPage/StyledComponents.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { Container };
