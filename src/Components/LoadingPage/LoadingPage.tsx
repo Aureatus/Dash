1c025bee68dba29e09967abe80ab15d1138fe39a
Alex Knott <alextknott@gmail.com>
2022-08-02 18:45:04 +0100

    Create loading page that renders loading content after 300ms

--- src/Components/LoadingPage/LoadingPage.tsx
+++ src/Components/LoadingPage/LoadingPage.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+
+import { Container, LoadingText, SpinningLogo } from './StyledComponents';
+
 const LoadingPage = () => {
-  return <div></div>;
+  const [pageContent, setPageContent] = useState<any>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPageContent(
+        <Container>
+          <SpinningLogo src="bolt-svgrepo-com.svg" />
+          <LoadingText>Loading...</LoadingText>
+        </Container>,
+      );
+    }, 300);
+    return () => clearTimeout(timer);
+  }, []);
+  return pageContent;
 };
 
 export default LoadingPage;
