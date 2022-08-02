3c081308e9b9501e3a3deb1951769fcb2803ef1b
Alex Knott <alextknott@gmail.com>
2022-08-02 16:25:52 +0100

    Add Sign in with google button

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -1,12 +1,32 @@
-import { Container, NavContainer, SignInLink, SignUpLink } from './StyledComponents';
+import { useTheme } from 'styled-components';
+
+import {
+  Container,
+  NavContainer,
+  SignInLink,
+  SignInWithGoogleDark,
+  SignInWithGoogleLight,
+  SignUpLink,
+} from './StyledComponents';
 
 const LandingPage = () => {
+  const theme = useTheme();
+
+  const themeChecker = () => {
+    if (theme.background.primary === '#ffffff') return 'light';
+    if (theme.background.primary === '#000000') return 'dark';
+  };
+
+  const themeName = themeChecker();
+
   return (
     <Container>
       <NavContainer>
         <SignUpLink to={'/sign-up'}>Sign Up</SignUpLink>
         <SignInLink to={'/sign-in'}>Sign In</SignInLink>
       </NavContainer>
+      {themeName === 'light' && <SignInWithGoogleLight />}
+      {themeName === 'dark' && <SignInWithGoogleDark />}
     </Container>
   );
 };
