9e69630edbb2790b349bb21b74cfc3752ca5e103
Alex Knott <alextknott@gmail.com>
2022-08-02 17:19:26 +0100

    Implement basic error handling for signOut and navigate to landing page after signOut

--- src/Components/DashHeader/LogOutButton.tsx
+++ src/Components/DashHeader/LogOutButton.tsx
@@ -1,4 +1,5 @@
 import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 import LogoutSVG from '/logout_FILL0_wght200_GRAD0_opsz40.svg';
 
@@ -6,10 +7,16 @@ import { auth } from '../../firebase/auth/auth';
 import { LogoutImg, StyledButton } from './StyledComponents';
 
 const LogOutButton = () => {
+  const navigate = useNavigate();
   return (
     <StyledButton
-      onClick={() => {
-        signOut(auth);
+      onClick={async () => {
+        try {
+          await signOut(auth);
+          navigate('/landing');
+        } catch (err) {
+          console.error(err);
+        }
       }}
     >
       <LogoutImg src={LogoutSVG} />
