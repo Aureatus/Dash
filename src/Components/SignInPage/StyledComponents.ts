2ceb048356fdef11ced1111655267a3bbda99bb2
Alex Knott <alextknott@gmail.com>
2022-08-03 20:16:14 +0100

    Make ShowPasswordButtonDark an adaptation of ShowPasswordButtonLight

--- src/Components/SignInPage/StyledComponents.ts
+++ src/Components/SignInPage/StyledComponents.ts
@@ -84,21 +84,6 @@ const StyledSubmitInput = styled.input`
   }
 `;
 
-const ShowPasswordButtonDark = styled.button`
-  height: 100%;
-  width: 5%;
-  min-height: 24px;
-  min-width: 24px;
-  padding: 0;
-  background-color: inherit;
-  border: none;
-  background-image: url('visibility_FILL0_wght400_GRAD0_opsz48.svg');
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  filter: invert(100%);
-`;
-
 const ShowPasswordButtonLight = styled.button`
   height: 100%;
   width: 5%;
@@ -111,6 +96,10 @@ const ShowPasswordButtonLight = styled.button`
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
+`;
+
+const ShowPasswordButtonDark = styled(ShowPasswordButtonLight)`
+  filter: invert(100%);
 `;
 
 export {
