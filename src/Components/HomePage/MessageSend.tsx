f092517752594175c3159782de1e4bd0c2d3c592
Alex Knott <alextknott@gmail.com>
2022-08-05 21:03:44 +0100

    Create component that lets you type a message.
    Message gets added as a document to firestore with the text content
    and user uid as fields.

--- src/Components/HomePage/MessageSend.tsx
+++ src/Components/HomePage/MessageSend.tsx
@@ -1,5 +1,42 @@
+import { addDoc, collection } from 'firebase/firestore';
+import { useContext, useState } from 'react';
+
+import UserContext from '../../context/UserContext';
+import { db } from '../../firebase/firestore/firestore';
+
 const MessageSend = () => {
-  return <div></div>;
+  const user = useContext(UserContext);
+  const [content, setContent] = useState('');
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        try {
+          addDoc(collection(db, 'messages'), {
+            content: content,
+            uid: user?.uid,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }}
+    >
+      <label>
+        <input
+          type="text"
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+          onInvalid={(e) => {
+            e.preventDefault();
+          }}
+          required
+          autoComplete="off"
+        />
+      </label>
+    </form>
+  );
 };
 
 export default MessageSend;
