5a0f89078f040929f38c7b1aa4206e3d0db0ee8c
Alex Knott <alextknott@gmail.com>
2022-08-05 14:43:45 +0100

    Add FormHeader to SignUpForm

--- src/Components/SignUpPage/SignUpPage.tsx
+++ src/Components/SignUpPage/SignUpPage.tsx
@@ -6,6 +6,7 @@ import { useTheme } from 'styled-components';
 import { auth } from '../../firebase/auth/auth';
 import {
   Container,
+  FormHeader,
   ShowPasswordButtonDark,
   ShowPasswordButtonLight,
   SignUpForm,
@@ -51,6 +52,7 @@ const SignUpPage = () => {
           EmailSignUp();
         }}
       >
+        <FormHeader>Sign Up</FormHeader>
         <StyledLabel>
           <StyledInput
             placeholder="email@gmail.com"
