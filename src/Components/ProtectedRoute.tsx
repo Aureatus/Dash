73620599c5aeb0797f73e921c8675d51f1ba175d
Alex Knott <alextknott@gmail.com>
2022-08-05 15:26:29 +0100

    Add capability to protect authentication routes from logged in users

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -5,20 +5,41 @@ import { Navigate, Outlet } from 'react-router-dom';
 const ProtectedRoute = ({
   currentUser,
   redirectPath,
+  desiredUserStatus,
   children,
 }: {
   currentUser: FirebaseUser | null;
   redirectPath: string;
+  desiredUserStatus: null | true;
   children?: ReactElement;
 }) => {
-  if (!currentUser) return <Navigate to={redirectPath} replace />;
-  return (
-    <>
-      {children ? children : <Outlet />}
-      {/*Renders children when ProtectedRoute is not
-      used as a Layout component. */}
-    </>
-  );
+  let componentJSX = <div></div>;
+  // Handles protection of authentication routes (sign-in, sign-up and landing)
+  if (desiredUserStatus === null) {
+    if (currentUser) componentJSX = <Navigate to={redirectPath} replace />;
+    else
+      componentJSX = (
+        <>
+          {children ? children : <Outlet />}
+          {/*Renders children when ProtectedRoute is not
+        used as a Layout component. */}
+        </>
+      );
+  }
+  // Handles protection of authenticated routes (home)
+  if (desiredUserStatus === true) {
+    if (!currentUser) componentJSX = <Navigate to={redirectPath} replace />;
+    else
+      componentJSX = (
+        <>
+          {children ? children : <Outlet />}
+          {/*Renders children when ProtectedRoute is not
+        used as a Layout component. */}
+        </>
+      );
+  }
+
+  return componentJSX;
 };
 
 export default ProtectedRoute;
