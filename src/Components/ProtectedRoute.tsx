add0e2ffe91404f2cb55046e5293869e34c80bda
Alex Knott <alextknott@gmail.com>
2022-08-05 17:57:12 +0100

    Remove unneeded bloat from code.
    No longer render children if present, only render outlet, and make
    general code more concise.

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,45 +1,24 @@
 import { User as FirebaseUser } from 'firebase/auth';
-import { ReactElement } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = ({
   currentUser,
   redirectPath,
   desiredUserStatus,
-  children,
 }: {
   currentUser: FirebaseUser | null;
   redirectPath: string;
   desiredUserStatus: null | true;
-  children?: ReactElement;
 }) => {
-  let componentJSX = <div></div>;
+  let componentJSX = <Outlet />;
   // Handles protection of authentication routes (sign-in, sign-up and landing)
-  if (desiredUserStatus === null) {
-    if (currentUser) componentJSX = <Navigate to={redirectPath} replace />;
-    else
-      componentJSX = (
-        <>
-          {children ? children : <Outlet />}
-          {/*Renders children when ProtectedRoute is not
-        used as a Layout component. */}
-        </>
-      );
-  }
-  // Handles protection of authenticated routes (home)
-  if (desiredUserStatus === true) {
-    if (!currentUser) componentJSX = <Navigate to={redirectPath} replace />;
-    else
-      componentJSX = (
-        <>
-          {children ? children : <Outlet />}
-          {/*Renders children when ProtectedRoute is not
-        used as a Layout component. */}
-        </>
-      );
-  }
+  if (desiredUserStatus === null && currentUser)
+    return <Navigate to={redirectPath} replace />;
 
-  return componentJSX;
+  // Handles protection of authenticated routes (home)
+  if (desiredUserStatus === true && !currentUser)
+    return <Navigate to={redirectPath} replace />;
+  else return componentJSX;
 };
 
 export default ProtectedRoute;
