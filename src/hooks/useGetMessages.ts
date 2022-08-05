95ce32026adfb16e83fd6f5cd7f5d8bcf8cf8743
Alex Knott <alextknott@gmail.com>
2022-08-05 20:12:58 +0100

    Create custom hook that gets firestore messages collection.
    Returns messages, loading and error. Messages is an array that contains
    an object for each message, with values for content, id and uid.

---
+++ src/hooks/useGetMessages.ts
@@ -0,0 +1,49 @@
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { Key, useEffect, useState } from 'react';
+
+import { db } from '../firebase/firestore/firestore';
+
+const useGetMessages = () => {
+  const [messages, setMessages] = useState<{ id: Key; content: String; uid: string }[]>(
+    [],
+  );
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    try {
+      const q = query(collection(db, 'messages'));
+
+      onSnapshot(
+        q,
+        (QuerySnapshot) => {
+          const messages: { id: Key; content: String; uid: string }[] = [];
+          QuerySnapshot.forEach((doc) => {
+            const content = doc.data().content;
+            const id: Key = doc.id;
+            const uid: string = doc.data().uid;
+            const message = { content: content, id: id, uid: uid };
+            messages.push(message);
+          });
+          setMessages(messages);
+        },
+        (error) => {
+          let message = 'Unknown Error';
+          if (error instanceof Error) message = error.message;
+          else message = String(error);
+          setError(message);
+        },
+      );
+    } catch (error) {
+      let message = 'Unknown Error';
+      if (error instanceof Error) message = error.message;
+      else message = String(error);
+      setError(message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+  return [messages, loading, error] as const;
+};
+
+export default useGetMessages;
