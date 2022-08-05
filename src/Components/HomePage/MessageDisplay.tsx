3e96802774fa2e8377401c03713a48b203fbfcfe
Alex Knott <alextknott@gmail.com>
2022-08-05 20:31:49 +0100

    Take messages as a prop and render message content and uid as bare html

--- src/Components/HomePage/MessageDisplay.tsx
+++ src/Components/HomePage/MessageDisplay.tsx
@@ -1,6 +1,21 @@
-const MessageDisplay = () => {
+import { Key } from 'react';
+
+const MessageDisplay = ({
+  messages,
+}: {
+  messages: { id: Key; content: String; uid: string }[];
+}) => {
   return (
-    
+    <div>
+      {messages.map((message) => {
+        return (
+          <div key={message.id}>
+            {message.content}
+            {message.uid}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
