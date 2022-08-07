e135cdb07b61023104b30ea27840036864c20ae8
Alex Knott <alextknott@gmail.com>
2022-08-07 16:27:39 +0100

    Create HomeContainer styled component

--- src/Components/HomePage/StyledComponents.ts
+++ src/Components/HomePage/StyledComponents.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
-export {};
+const HomeContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export { HomeContainer };
