b7fd02ee2add2191997c39f4571c1eeae100c617
Alex Knott <alextknott@gmail.com>
2022-08-07 16:09:48 +0100

    Return messages and an input to send messages through

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,5 +1,18 @@
+import useGetMessages from '../../hooks/useGetMessages';
+import MessageDisplay from './MessageDisplay';
+import MessageSend from './MessageSend';
+
 const HomePage = () => {
-  return <div></div>;
+  const [messages, loading, error] = useGetMessages();
+
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>{error}</div>;
+  return (
+    <div>
+      <MessageDisplay messages={messages} />
+      <MessageSend />
+    </div>
+  );
 };
 
 export default HomePage;
