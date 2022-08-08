0482aacc089031599a8bc8c79a124603829f7f0b
Alex Knott <alextknott@gmail.com>
2022-08-08 21:22:50 +0100

    Render user profilePicture beside message content

--- src/Components/HomePage/MessageDisplay.tsx
+++ src/Components/HomePage/MessageDisplay.tsx
@@ -1,17 +1,34 @@
 import { Timestamp } from 'firebase/firestore';
 import { Key } from 'react';
 
-import { Message, MessageDisplayContainer } from './StyledComponents';
+import { Message, MessageDisplayContainer, ProfilePicture } from './StyledComponents';
 
 const MessageDisplay = ({
   messages,
 }: {
-  messages: { id: Key; content: String; uid: string; timestamp: Timestamp }[];
+  messages: {
+    id: Key;
+    content: String;
+    uid: string;
+    timestamp: Timestamp;
+    profilePicture: string | null;
+  }[];
 }) => {
   return (
     <MessageDisplayContainer>
       {messages.map((message) => {
-        return <Message key={message.id}>{message.content}</Message>;
+        return (
+          <Message key={message.id}>
+            {message.content}
+            <ProfilePicture
+              src={
+                message.profilePicture
+                  ? message.profilePicture
+                  : 'https://cdn-icons.flaticon.com/png/512/3177/premium/3177440.png?token=exp=1659984790~hmac=5640feba3b7768fc54406a6105415c39'
+              }
+            />
+          </Message>
+        );
       })}
     </MessageDisplayContainer>
   );
