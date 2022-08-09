58df1c3f8581fda1dbb5f902f9e691b78236f0e3
Alex Knott <alextknott@gmail.com>
2022-08-09 16:01:25 +0100

    Update getMessageTime helper
    Now displays Yesterday/Today with a time, and any other day as a date.
    Made code more concise.

--- src/Utilities/getMessageTime.ts
+++ src/Utilities/getMessageTime.ts
@@ -1,12 +1,30 @@
 const getMessageTime = (messageSeconds: number) => {
-  const currentTime = Date.now();
-  const timeDifference = currentTime / 1000 - messageSeconds;
-  if (timeDifference > 86400) {
-    const time = new Date(messageSeconds * 1000).toLocaleDateString();
-    return time;
+  const messageMilliseconds = messageSeconds * 1000;
+  const messageDate = new Date(messageMilliseconds);
+  const currentDate = new Date();
+  const yesterday = new Date(currentDate);
+
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  if (messageDate.toDateString() === yesterday.toDateString()) {
+    const date = new Date(messageMilliseconds);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const time = hours + ':' + minutes;
+    return { prefix: 'Yesterday at ', time: time };
+  }
+
+  if (messageDate.toDateString() === currentDate.toDateString()) {
+    const date = new Date(messageMilliseconds);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const time = hours + ':' + minutes;
+    return { prefix: 'Today at ', time: time };
   } else {
-    const time = new Date(messageSeconds * 1000).toISOString().substring(11, 16);
-    return time;
+    const time = new Date(messageMilliseconds).toLocaleDateString();
+    return { prefix: '', time: time };
   }
 };
 
