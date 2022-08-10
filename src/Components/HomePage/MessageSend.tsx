6ff93d97aebb3c412ded482d18da157b6ff44ce4
Alex Knott <alextknott@gmail.com>
2022-08-10 17:32:36 +0100

    Implement a basic profanity filter using censor-sensor

--- src/Components/HomePage/MessageSend.tsx
+++ src/Components/HomePage/MessageSend.tsx
@@ -1,17 +1,37 @@
+import { CensorSensor } from 'censor-sensor';
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import UserContext from '../../context/UserContext';
 import { db } from '../../firebase/firestore/firestore';
-import { StyledForm, StyledInput, StyledLabel } from './StyledComponents';
+import {
+  InputErrorMessage,
+  StyledForm,
+  StyledInput,
+  StyledLabel,
+} from './StyledComponents';
 
 const MessageSend = () => {
   const user = useContext(UserContext);
   const [content, setContent] = useState('');
+  const [displayProfanityWarning, setDisplayProfanityWarning] = useState(false);
+  const censor = new CensorSensor();
+  censor.disableTier(4); // Disables possibly offensive terms
+  censor.disableTier(2); // Disables common profanity
+
+  useEffect(() => {
+    if (censor.isProfane(content)) {
+      setDisplayProfanityWarning(true);
+    } else {
+      if (!censor.isProfane(content)) setDisplayProfanityWarning(false);
+    }
+  }, [content]);
+
   return (
     <StyledForm
       onSubmit={(e) => {
         e.preventDefault();
+        if (censor.isProfane(content)) return;
         try {
           addDoc(collection(db, 'messages'), {
             content: content,
@@ -39,6 +59,9 @@ const MessageSend = () => {
           autoComplete="off"
           placeholder="Type message"
         />
+        {displayProfanityWarning && (
+          <InputErrorMessage>Please remove profane content.</InputErrorMessage>
+        )}
       </StyledLabel>
     </StyledForm>
   );
