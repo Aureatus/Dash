a141767bd2d5318ff007ae0a86326537400b761a
Alex Knott <alextknott@gmail.com>
2022-08-11 12:59:16 +0100

    Implement lazy loading for most component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { lazy, Suspense, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import DashHeader from './Components/DashHeader/DashHeader';
-import HomePage from './Components/HomePage/HomePage';
-import LandingPage from './Components/LandingPage/LandingPage';
+const DashHeader = lazy(() => import('./Components/DashHeader/DashHeader'));
+const HomePage = lazy(() => import('./Components/HomePage/HomePage'));
+const LandingPage = lazy(() => import('./Components/LandingPage/LandingPage'));
 import LoadingPage from './Components/LoadingPage/LoadingPage';
-import ProtectedRoute from './Components/ProtectedRoute';
-import SignInPage from './Components/SignInPage/SignInPage';
-import SignUpPage from './Components/SignUpPage/SignUpPage';
+const ProtectedRoute = lazy(() => import('./Components/ProtectedRoute'));
+const SignInPage = lazy(() => import('./Components/SignInPage/SignInPage'));
+const SignUpPage = lazy(() => import('./Components/SignUpPage/SignUpPage'));
 import UserContext from './context/UserContext';
 import GlobalStyle from './GlobalStyle';
 import useAuthState from './hooks/useAuthState';
@@ -25,30 +25,34 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <UserContext.Provider value={user}>
-        <Container>
-          <DashHeader setTheme={setTheme}></DashHeader>
-          {loading ? (
-            <LoadingPage text="User loading..." />
-          ) : (
-            <Routes>
-              <Route
-                element={<ProtectedRoute currentUser={user} desiredUserStatus={null} />}
-              >
-                <Route index element={<Navigate to={'/landing'} />} />
-                <Route path="landing" element={<LandingPage />} />
-                <Route path="sign-in" element={<SignInPage />} />
-                <Route path="sign-up" element={<SignUpPage />} />
-              </Route>
-              <Route
-                element={<ProtectedRoute currentUser={user} desiredUserStatus={!null} />}
-              >
-                <Route path="home" element={<HomePage />} />
-              </Route>
-            </Routes>
-          )}
-        </Container>
-      </UserContext.Provider>
+      <Suspense fallback={<LoadingPage text="Loading..." />}>
+        <UserContext.Provider value={user}>
+          <Container>
+            <DashHeader setTheme={setTheme}></DashHeader>
+            {loading ? (
+              <LoadingPage text="User loading..." />
+            ) : (
+              <Routes>
+                <Route
+                  element={<ProtectedRoute currentUser={user} desiredUserStatus={null} />}
+                >
+                  <Route index element={<Navigate to={'/landing'} />} />
+                  <Route path="landing" element={<LandingPage />} />
+                  <Route path="sign-in" element={<SignInPage />} />
+                  <Route path="sign-up" element={<SignUpPage />} />
+                </Route>
+                <Route
+                  element={
+                    <ProtectedRoute currentUser={user} desiredUserStatus={!null} />
+                  }
+                >
+                  <Route path="home" element={<HomePage />} />
+                </Route>
+              </Routes>
+            )}
+          </Container>
+        </UserContext.Provider>
+      </Suspense>
     </ThemeProvider>
   );
 }
