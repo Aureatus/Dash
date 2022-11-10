d556b72c3f4822dace16db029c269e7161b6bb5a
Alex Knott <alextknott@gmail.com>
2022-11-10 16:06:25 +0000

    Update to react 18 client rendering API

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import 'normalize.css';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
