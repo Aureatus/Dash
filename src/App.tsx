a65e07deee2d1748703f55f8dfbf2881445a2f87
Alex Knott <alextknott@gmail.com>
2022-11-10 18:32:11 +0000

    Utilise loaders to protect routes, over a protector route

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import { ThemeProvider } from 'styled-components';
 import DashHeader from './Components/DashHeader/DashHeader';
 import HomePage from './Components/HomePage/HomePage';
 import LandingPage from './Components/LandingPage/LandingPage';
-import ProtectedRoute from './Components/ProtectedRoute';
 import SignInPage from './Components/SignInPage/SignInPage';
 import SignUpPage from './Components/SignUpPage/SignUpPage';
 import UserContext from './context/UserContext';
@@ -20,10 +19,11 @@ import GlobalStyle from './GlobalStyle';
 import useAuthState from './hooks/useAuthState';
 import { Container } from './StyledComponents';
 import { lightTheme } from './themes';
+import homeLoader from './Utilities/loaders/homeLoader';
+import landingLoader from './Utilities/loaders/landingLoader';
 
 function App() {
   const [theme, setTheme] = useState(lightTheme);
-
   const [user, loading, error] = useAuthState();
 
   if (loading) return null;
@@ -32,33 +32,31 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route element={<ProtectedRoute currentUser={user} desiredUserStatus={null} />}>
-          <Route index element={<Navigate to={'/landing'} />} />
-          <Route
-            element={
-              <>
-                <DashHeader setTheme={setTheme} />
-                <Outlet />
-              </>
-            }
-          >
-            <Route path="landing" element={<LandingPage />} />
-            <Route path="sign-in" element={<SignInPage />} />
-            <Route path="sign-up" element={<SignUpPage />} />
-          </Route>
+        <Route index element={<Navigate to={user ? 'home' : '/landing'} />} />
+        <Route
+          element={
+            <>
+              <DashHeader setTheme={setTheme} />
+              <Outlet />
+            </>
+          }
+          loader={() => landingLoader(user)}
+        >
+          <Route path="landing" element={<LandingPage />} />
+          <Route path="sign-in" element={<SignInPage />} />
+          <Route path="sign-up" element={<SignUpPage />} />
         </Route>
-        <Route element={<ProtectedRoute currentUser={user} desiredUserStatus={!null} />}>
-          <Route
-            path="home"
-            element={
-              <>
-                <DashHeader setTheme={setTheme} />
-                <Outlet />
-              </>
-            }
-          >
-            <Route index element={<HomePage />} />
-          </Route>
+        <Route
+          path="home"
+          element={
+            <>
+              <DashHeader setTheme={setTheme} />
+              <Outlet />
+            </>
+          }
+          loader={() => homeLoader(user)}
+        >
+          <Route index element={<HomePage />} />
         </Route>
       </Route>,
     ),
