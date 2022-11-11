9592b8a4d2eb18abce4d15e0984cd68a977f7fcb
Alex Knott <alextknott@gmail.com>
2022-11-11 10:27:24 +0000

    Revert "Delete unused ProtectedRoute"
    This reverts commit a6f328e9bfdb1d5f799ccbd2dfc9ec050187a678.

---
+++ src/Components/ProtectedRoute.tsx
@@ -0,0 +1,35 @@
+import { User as FirebaseUser } from 'firebase/auth';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+const ProtectedRoute = ({
+  currentUser,
+  desiredUserStatus,
+}: {
+  currentUser: FirebaseUser | null;
+  desiredUserStatus: null | true;
+}) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Navigates back to last location in history stack
+    if (desiredUserStatus === null && currentUser) {
+      if (window.history.length <= 2) {
+        navigate('/home', { replace: true });
+      } else navigate(-1);
+    }
+    if (desiredUserStatus === true && !currentUser)
+      if (window.history.length <= 2) {
+        navigate('/landing', { replace: true });
+      } else navigate(-1);
+  }, []);
+
+  // Handles protection of authentication routes (sign-in, sign-up and landing)
+  if (desiredUserStatus === null && currentUser) return null;
+
+  // Handles protection of authenticated routes (home)
+  if (desiredUserStatus === true && !currentUser) return null;
+  else return <Outlet />;
+};
+
+export default ProtectedRoute;
