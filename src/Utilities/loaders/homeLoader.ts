ae2b9a7fa42dd78faa2ce2df3de959b535f8f63f
Alex Knott <alextknott@gmail.com>
2022-11-11 10:26:42 +0000

    Revert "Create loaders for protecting routes"
    This reverts commit cc826ddcc685bed79257435fba38a02714322812.

--- src/Utilities/loaders/homeLoader.ts
+++
@@ -1,8 +0,0 @@
-import { User as FirebaseUser } from 'firebase/auth';
-import { redirect } from 'react-router-dom';
-
-export default function homeLoader(user: FirebaseUser | null) {
-  if (!user)
-    if (window.history.length <= 2) return redirect('/landing');
-    else return redirect('/landing');
-}
--- src/Utilities/loaders/landingLoader.ts
+++
@@ -1,9 +0,0 @@
-import { User as FirebaseUser } from 'firebase/auth';
-import { redirect } from 'react-router-dom';
-
-export default function landingLoader(user: FirebaseUser | null) {
-  if (user) {
-    if (window.history.length <= 2) return redirect('/home');
-    else return redirect('/home');
-  }
-}
